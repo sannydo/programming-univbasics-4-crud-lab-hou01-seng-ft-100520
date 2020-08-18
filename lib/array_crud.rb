5e92a20d9b15928177d47142083cd15032797ea0
Sanny Do <sanny.do94@gmail.com>
2020-08-18 01:59:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,7 +37,8 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  array[index_number]
   array[0]
+  array[index_number]
+  
   
 end
